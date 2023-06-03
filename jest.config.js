65c3b98e9e49fecc73fd11761ae3809c157dc023
andyevers <aevers@themeshark.com>
2023-06-03 09:09:27 -0500

    updated package.json, updated jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    coverageReporters: ["text-summary"],
-    collectCoverage: true,
-    verbose: false,
-    testMatch: ["/**/*.test.ts"],
-    rootDir: "./",
-    moduleFileExtensions: ["ts", "js"],
-};
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	coverageReporters: ['text-summary'],
+	collectCoverage: true,
+	verbose: false,
+	testMatch: ['/**/*.test.ts'],
+	rootDir: './',
+	moduleFileExtensions: ['ts', 'js'],
+}
